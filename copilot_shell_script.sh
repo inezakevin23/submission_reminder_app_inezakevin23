38a34ee701697d715e08a2eb914b76dae19722fb
Ineza Kevin <k.ineza1.alustudent.com>
2025-06-18 05:17:59 -0700

    updating and doing some changes to both scripts

--- copilot_shell_script.sh
+++ copilot_shell_script.sh
@@ -4,6 +4,8 @@
 set -e
 
 read -p "Hello, please enter your name: " username
+#capitalising first letter of username for handling user typing error
+username=$(echo "$username" | sed -E 's/(^)([a-z])/\1\u\2/g')
 
 #path to the expected directory
 dir="./submission_reminder_$username"
@@ -11,18 +13,18 @@ dir="./submission_reminder_$username"
 #checking if directory exist
 echo "checking if directory exist ..."
 if [[ ! -d "$dir" ]]; then
-	echo "$dir directory does not exist. Please check your name or set up the app first"
+	echo "$dir directory does not exist. Please check your name or set up the app first by running create_environment.sh"
 	exit 1
 fi
 
 # Determining greeting based on time of day
 hour=$(date +"%H")
 if [ "$hour" -lt 12 ]; then
-	greeting="Good morning"
+    greeting="Good morning"
 elif [ "$hour" -lt 17 ]; then
-	greeting="Good afternoon"
+    greeting="Good afternoon"
 else
-	greeting="Good evening"
+    greeting="Good evening"
 fi
 
 read -p "$greeting, $username please enter the assignment name: " AssignmentName
@@ -30,10 +32,10 @@ read -p "$greeting, $username please enter the assignment name: " AssignmentName
 #capitalising first letter of assignmentname for handling user typing error
 AssignmentName=$(echo "$AssignmentName" | sed -E 's/(^| )([a-z])/\1\u\2/g')
 
-# Checking if assignment exists in submissions.txt
+#checking if assignment exists in submissions.txt
 if ! grep -q ", $AssignmentName," "$dir/assets/submissions.txt"; then
-	echo "$AssignmentName assignment does not exist, please check the spelling."
-	exit 1
+       echo "$AssignmentName assignment does not exist, please check the spelling."
+       exit 1
 fi
 
 # Updating ASSIGNMENT in config.env
@@ -43,4 +45,3 @@ echo "---------------------------------------------------"
 
 #running the app
 bash "$dir/startup.sh"
-
